79688316830da3f1c685727c74fe71f9903a6696
Ryan Delos Santos <ryansantos.delossantos121@gmail.com>
2022-01-11 14:36:56 -0600

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = function(array){
+    return [array[0], array[1]];
+}
+
+const returnLastTwoDrivers = function(array){
+    return [array[array.length - 2], array[array.length - 1]];
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+
+function createFareMultiplier(fareMultiplier){
+
+    return function(farenum){
+        return farenum * fareMultiplier;
+    }
+}
+
+
+function fareDoubler(fare){
+    return createFareMultiplier(2)(fare);
+}
+
+function fareTripler(fare){
+    return createFareMultiplier(3)(fare);
+}
+
+function selectDifferentDrivers(array, cbf){
+    return cbf(array);
+}
